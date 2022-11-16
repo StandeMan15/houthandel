6c855e8d05c3cde49f18f14f202b1e42d01f1811
Sander Bunschoten <558088@edu.rocmn.nl>
2022-11-16 12:45:38 +0100

    update to check

--- form.php
+++ form.php
@@ -8,13 +8,13 @@
 
 
     <label for="class_two">Klasse 2</label>
-    <input type="radio" name="class_two" id="class_two"><br>
+    <input type="checkbox" name="class_two" id="class_two"><br>
 
     <label for="capacity">Hoeveel m3</label>
     <input type="text" name="capacity" id="capacity_two"><br><br>
 
     <label for="class_three">Klasse 3</label>
-    <input type="radio" name="class_three" id="class_three"><br>
+    <input type="checkbox" name="class_three" id="class_three"><br>
 
     <label for="capacity">Hoeveel m3</label>
     <input type="text" name="capacity" id="capacity_three"><br><br>     
