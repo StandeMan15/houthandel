729f6b5176a59428b4851e620a48f943c50b4d5b
Sander Bunschoten <558088@edu.rocmn.nl>
2022-11-16 13:00:30 +0100

    calc

--- form_handler.php
+++ form_handler.php
@@ -3,12 +3,22 @@
 $class_one = $_REQUEST['class_one'];
 $class_two = $_REQUEST['class_two'];
 $class_three = $_REQUEST['class_three'];
+$total = 0;
 
-$cap_one = $_REQUEST['capacity_one'];
-$cap_two = $_REQUEST['capacity_two'];
-$cap_three = $_REQUEST['capacity_three'];
+if(isset($class_one)){
+    $amount_one = $class_one * 300;
+    $total += $amount_one;
+}
 
-$total = 3*3;
+if(isset($class_two)){
+    $amount_two = $class_two * 300;
+    $total += $amount_one;
+}
+
+if(isset($class_three)){
+    $amount_three = $class_three * 300;
+    $total += $amount_three;
+}
 
 echo $total;
 
