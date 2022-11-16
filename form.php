90f746173b8f228df3aaa3fee917a11748795735
StandeMan15 <99266225+StandeMan15@users.noreply.github.com>
2022-11-16 12:44:11 +0100

    form update

--- form.php
+++ form.php
@@ -1,15 +1,23 @@
 <form action="form_handler.php" method="post">
+
     <label for="class_one">Klasse 1</label>
-    <input type="radio" name="class_one" id="class_one"><br>
+    <input type="checkbox" name="class_one" id="class_one"><br>
+
+    <label for="capacity">Hoeveel m3</label>
+    <input type="text" name="capacity" id="capacity_one"><br><br>
+
 
     <label for="class_two">Klasse 2</label>
     <input type="radio" name="class_two" id="class_two"><br>
+
+    <label for="capacity">Hoeveel m3</label>
+    <input type="text" name="capacity" id="capacity_two"><br><br>
 
     <label for="class_three">Klasse 3</label>
     <input type="radio" name="class_three" id="class_three"><br>
 
     <label for="capacity">Hoeveel m3</label>
-    <input type="text" name="capacity" id="capacity"><br>
+    <input type="text" name="capacity" id="capacity_three"><br><br>     
 
     <input type="submit" value="Submit">
 </form>
