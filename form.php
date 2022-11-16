31680c355b340cbbff6b59d6a811887d843b4515
StandeMan15 <99266225+StandeMan15@users.noreply.github.com>
2022-11-16 12:38:52 +0100

    form

---
+++ form.php
@@ -0,0 +1,15 @@
+<form action="form_handler.php" method="post">
+    <label for="class_one">Klasse 1</label>
+    <input type="radio" name="class_one" id="class_one"><br>
+
+    <label for="class_two">Klasse 2</label>
+    <input type="radio" name="class_two" id="class_two"><br>
+
+    <label for="class_three">Klasse 3</label>
+    <input type="radio" name="class_three" id="class_three"><br>
+
+    <label for="capacity">Hoeveel m3</label>
+    <input type="text" name="capacity" id="capacity"><br>
+
+    <input type="submit" value="Submit">
+</form>
