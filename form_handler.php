9b04c75ac82b37638fea1bbc445b8dc5d11bef9f
Sander Bunschoten <558088@edu.rocmn.nl>
2022-11-16 12:53:02 +0100

    calc test

--- form_handler.php
+++ form_handler.php
@@ -1,2 +1,15 @@
 <?php
 
+$class_one = $_REQUEST['class_one'];
+$class_two = $_REQUEST['class_two'];
+$class_three = $_REQUEST['class_three'];
+
+$cap_one = $_REQUEST['capacity_one'];
+$cap_two = $_REQUEST['capacity_two'];
+$cap_three = $_REQUEST['capacity_three'];
+
+$total = 3*3;
+
+echo $total;
+
+?>
